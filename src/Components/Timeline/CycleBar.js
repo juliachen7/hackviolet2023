9f72d56681462bdeac6e8bf4174654126edefa0e
AlexWallace2001 <alexw20@vt.edu>
2023-02-04 17:39:12 -0500

    added images to timeline bar

--- src/Components/Timeline/CycleBar.js
+++ src/Components/Timeline/CycleBar.js
@@ -1,7 +1,8 @@
 import React from 'react'
   
+let BloodLogo = require('./blood.png')
 const ProgressBar = ({progressColor,backColor,progress,height}) => {
-     
+      
     const Parentdiv = {
         height: height,
         width: '93%',
@@ -17,17 +18,18 @@ const ProgressBar = ({progressColor,backColor,progress,height}) => {
        borderRadius:40,
         textAlign: 'right'
       }
-      
-      const progresstext = {
+
+      /*const progresstext = {
         padding: 10,
         color: 'black',
         fontWeight: 900
-      }
         
+      }*/
+      //<span style={progresstext}>{`O`}</span> goes in the middle of the inner div below
     return (
     <div style={Parentdiv}>
       <div style={Childdiv}>
-        <span style={progresstext}>{`O`}</span>
+        <img src={BloodLogo} alt="O" width="25" height="30"></img>
       </div>
     </div>
     )
--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -5,7 +5,7 @@ import ProgressBar from './CycleBar.js'
 class Timeline extends React.Component {
   render() {
     return <div className="Timeline">
-        <ProgressBar progressColor="red" backColor="black" progress='30'  height={30}></ProgressBar>
+        <ProgressBar progressColor="darkturquoise" backColor="lavender" progress='30'  height={30}></ProgressBar>
           <header className="Timeline-header">
             <p>
               test - Edit <code>src/Timeline.js</code> and save to reload.
