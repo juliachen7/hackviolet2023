a8b08f4dc750237d4477b03be37c348516034885
DarrenZ12 <darrenzheng1@gmail.com>
2023-02-04 16:33:43 -0500

    timeline file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Timeline from './Components/Timeline/Timeline.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test2 - Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timeline></Timeline>
     </div>
   );
 }
--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './Timeline.css';
+import React from 'react';
 
-function Timeline() {
-  return (
-    <div className="Timeline">
-      <header className="Timeline-header">
-        <img src={logo} className="Timeline-logo" alt="logo" />
-        <p>
-          test2 - Edit <code>src/Timeline.js</code> and save to reload.
-        </p>
-        <a
-          className="Timeline-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timeline extends React.Component {
+  render() {
+    return <div className="Timeline">
+          <header className="Timeline-header">
+            <p>
+              test - Edit <code>src/Timeline.js</code> and save to reload.
+            </p>
+            <a
+              className="Timeline-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      ;
+  }
+   
 }
+
+export default Timeline;
