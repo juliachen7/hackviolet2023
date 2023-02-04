0e6686d1ef0d5faddc4529880af9148a1b03adb5
GawZhangX <gawzhang@icloud.com>
2023-02-04 18:08:14 -0500

    added background folder

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './Background.css';
+
+
+class Background extends React.Component { 
+    render() {
+ return (
+   <div class="ocean">
+ <div class="wave"></div>
+</div>
+  
+ );
+}
+}
+
+export default Background;
+
+
