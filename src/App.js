a983cae703b296aead28fe792051ae65718c0705
GawZhangX <gawzhang@icloud.com>
2023-02-04 18:02:17 -0500

    added background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Timeline from './Components/Timeline/Timeline.js';
+import Background from './Components/Background/Background.js';
+
 
 function App() {
   return (
     <div className="App">
       <Timeline></Timeline>
+      <Background></Background>
     </div>
   );
 }
