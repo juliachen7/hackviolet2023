ebdedf8335a165b39a2782b4ad041a8b7b8f5fbe
GawZhangX <gawzhang@icloud.com>
2023-02-04 20:23:18 -0500

    updated colors

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -1,19 +1,18 @@
-
 import React from 'react';
 import './Background.css';
 
 
-class Background extends React.Component { 
-    render() {
- return (
-   <div class="ocean">
- <div class="wave"></div>
+
+
+class Background extends React.Component {
+   render() {
+return (
+  <div class="ocean">
+<div class="wave"></div>
 </div>
-  
  );
 }
 }
 
+
 export default Background;
-
-
