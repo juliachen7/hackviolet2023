f9dd9b15dd4ff7d16d2fc9865968bed63730171d
juliachen <juliachen@vt.edu>
2023-02-04 23:28:12 -0500

    Put tracker, lists, and summary inside of MainLayout Component

--- src/App.js
+++ src/App.js
@@ -6,29 +6,13 @@ import Grid from '@mui/material/Grid';
 import FoodList from "./Components/FoodList/FoodList";
 import FoodPlaces from "./Components/FoodPlaces/FoodPlaces";
 import About from './Components/About/About.js';
+import MainLayout from "./Components/MainLayout/MainLayout";
 
 function App() {
   return (
     <div className="App">
       <Timeline></Timeline>
-        <Grid container spacing={2}>
-            <Grid item xs={5} md={5}>
-                <Grid container spacing={0}>
-                    <Grid item xs={12} md={12}>
-                        <FoodList></FoodList>
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                        <FoodPlaces></FoodPlaces>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={2} md={2}>
-                <p>Summary</p>
-            </Grid>
-            <Grid item xs={5} md={5}>
-                <Tracker></Tracker>
-            </Grid>
-        </Grid>
+        <MainLayout></MainLayout>
       <Background></Background>
       <About></About>
     </div>
