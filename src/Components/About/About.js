4d8ad9a85af10d561fb4d0ec6988182033941190
DarrenZ12 <darrenzheng1@gmail.com>
2023-02-04 20:01:31 -0500

    Added basic backend as well as initial about page

---
+++ src/Components/About/About.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Timeline from './Components/Timeline/Timeline.js';
+import Background from './Components/Background/Background.js';
+
+
+function App() {
+  return (
+    <div className="App">
+        
+      <Timeline></Timeline>
+      <Background></Background>
+    </div>
+  );
+}
+
+export default App;
