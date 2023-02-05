d67adf56fd364d3d3226763686d8f67463f8727e
DarrenZ12 <darrenzheng1@gmail.com>
2023-02-05 04:12:15 -0500

    Merge Conflicts resolved, fixed Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,13 +3,15 @@ import './Home.css';
 import Timeline from '../Timeline/Timeline.js';
 import Background from '../Background/Background.js';
 import Layout from "../Layout/Layout";
+import MainLayout from "../MainLayout/MainLayout";
 
 export default function Home() {
     console.log('test')
     return (
         <div>
             <Layout></Layout>
-            <Timeline></Timeline>
+            <MainLayout></MainLayout>
+            {/* <Timeline></Timeline> */}
             <Background></Background>
         </div>
     )
