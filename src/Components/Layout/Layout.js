d8240794c6b673a7eb1f9918e24a43a06fc9000d
DarrenZ12 <darrenzheng1@gmail.com>
2023-02-05 06:20:38 -0500

    google auth

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -3,15 +3,28 @@ import './Layout.css';
 import {ReactComponent as CogIcon } from '../../Icons/cog.svg';
 import {ReactComponent as LogoutIcon } from '../../Icons/logout.svg';
 import {ReactComponent as AboutIcon } from '../../Icons/about.svg';
-
+import { GoogleLogin } from '@react-oauth/google';
 
 export default function Layout() {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
     return (
+      <div>
         <Navbar>
-            <NavItem icon={<CogIcon/>}/>
-            <NavItem icon={<AboutIcon/>}/>
-            <NavItem icon={<LogoutIcon/>}/>
+            <div id='google'>
+              <NavItem icon={<GoogleLogin onSuccess={responseMessage} onError={errorMessage} />} path="/settings"/>
+            </div>
+            <NavItem icon={<AboutIcon/>} path="/about"/>
+            <NavItem icon={<LogoutIcon/>} path="/test"/>
         </Navbar>
+        <div>
+          
+        </div>
+      </div>
     )
 }
 
@@ -28,7 +41,7 @@ function Navbar(props) {
 function NavItem(props) {
     return (
         <li className='nav-item'>
-            <a href='#' className='icon-button'>
+            <a href={props.path} className='icon-button'>
                 {props.icon}
             </a>
         </li>
